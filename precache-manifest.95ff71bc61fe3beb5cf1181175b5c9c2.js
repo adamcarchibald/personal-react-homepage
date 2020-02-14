f93c97f1e345a6ef3fedb1ace244e8df202baf2c
Adam Archibald <adam.archibald@shopify.com>
2020-02-13 21:40:28 -0500

    Updates

---
+++ precache-manifest.95ff71bc61fe3beb5cf1181175b5c9c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5e6b7f4e01022051e3b44112b87c18c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "097c83e305a4982c6406",
+    "url": "/static/css/main.b2b7e812.chunk.css"
+  },
+  {
+    "revision": "2ff6ed803287ec3c9299",
+    "url": "/static/js/2.a6363408.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.a6363408.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "097c83e305a4982c6406",
+    "url": "/static/js/main.f9ac23d1.chunk.js"
+  },
+  {
+    "revision": "f92b379501707a785e17",
+    "url": "/static/js/runtime-main.bf4ce1d1.js"
+  },
+  {
+    "revision": "53a8252efd33c25bc8ffd4be51bef742",
+    "url": "/static/media/robot-logo-v2.53a8252e.svg"
+  }
+]);
